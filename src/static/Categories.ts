f1afec62a48870c8abfc7598c07759c9178ad42a
VineethNareddy <nareddyvineeth@gmail.com>
2022-10-15 02:05:47 -0400

    added category

---
+++ src/static/Categories.ts
@@ -0,0 +1,10 @@
+﻿export const categories = [{name: "Career & Work", description: "The name of this section will depend on what is applicable to you now. Use Career for workers, Business for businesspeople, and Studies for college students. This is regarded as a key section because job and school take up a significant percentage of our waking hours."},
+{name: "Money & Finances", description: "How are your financial circumstances?"},
+{name: "Health & Fitness", description: "Both your lifestyle and physical health. This includes things like exercise, diet, and rest."},
+{name: "Fun & Recreation", description: "Whether you have enough opportunity to pursue your interests and hobbies outside of work."},
+{name: "Contribution", description: "how much you're donating. how you respect the environment and the planet. Here, social causes are included."},
+{name: "Community/Social", description: "how your social life is going. Have you got friends? Do you have positive, close friends with whom you can discuss anything?"},
+{name: "Family & Friends", description: "Your interactions with your family."},
+{name: "Partner & Love", description: "This reflects your satisfaction with your relationship, if you're in one. If you're single, it symbolizes how much love you feel in your life and how energetically you're living it. Simply being single does not guarantee that you will receive a score of zero out of ten; Like that, being with someone does not guarantee that you will receive a ten out of ten. Both a single person and someone in a relationship can give themselves good or negative ratings in this section."},
+{name: "Personal Growth & Self-image", description: "your perception of yourself. This covers how you feel about yourself physically and how confident you are."},
+{name: "Spirituality", description: "your relationship to the universe and your higher self. This is not related to being religious, despite popular notion. How much do you connect with the spiritual side of life?"}]
