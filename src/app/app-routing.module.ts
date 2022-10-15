ce0b087608e1edf335b1c5f54a54cf5c74784cb4
VineethNareddy <nareddyvineeth@gmail.com>
2022-10-15 08:59:24 -0400

    pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CategoryComponent } from 'src/components/category/category.component';
+import { CategoriesComponent } from './components/categories/categories.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'category', component:CategoryComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
