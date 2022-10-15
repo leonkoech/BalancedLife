001cf332a91d91969c36bb3bd60e3570cb89b0ef
VineethNareddy <nareddyvineeth@gmail.com>
2022-10-15 14:03:35 -0400

    vineeth

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,7 +7,7 @@ import { LoginComponent } from './login/login.component';
 const routes: Routes = [
   {path: '', component:LoginComponent},
   {path: 'category', component:CategoryComponent},
-  {path: 'categoryPage', component:CategoryPageComponent}
+  {path: 'categoryPage/:name', component:CategoryPageComponent}
 ];
 
 @NgModule({
--- src/app/pages/category-page/category-page.component.ts
+++ src/app/pages/category-page/category-page.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-category-page',
@@ -7,9 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CategoryPageComponent implements OnInit {
 
-  constructor() { }
+  constructor(private route: ActivatedRoute) { }
 
   ngOnInit(): void {
+    this.route.params.subscribe(params => {
+      console.log(params["name"]); 
+    });
   }
+  @Input name: string;
 
 }
