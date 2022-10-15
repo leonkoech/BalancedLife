23e98fc579a91cffde3d14e97df5477d7f45c409
VineethNareddy <nareddyvineeth@gmail.com>
2022-10-15 08:10:21 -0400

    categoories

--- src/components/category/category.component.ts
+++ src/components/category/category.component.ts
@@ -1,26 +1,21 @@
 import { Component, OnInit } from '@angular/core';
 import { categories } from '../../../src/static/Categories';
 
-
-
-
 @Component({
   selector: 'app-category',
   templateUrl: './category.component.html',
   styleUrls: ['./category.component.scss']
 })
 export class CategoryComponent implements OnInit {
-
-  constructor() { }
+   test=categories
+  constructor(
+    
+  ) { }
 
   ngOnInit(): void {
-    this.print()
   }
-   print(){
-    console.log("testete")
-    for (let i = 0; i < categories.length; i++){
-    console.log (categories[i]["name"])
-  }
+   doNext(object:any){
+    console.log(object.name)
   }
   
 
